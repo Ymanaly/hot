f46a4be5b7cc188ebca10efc52b5e7ecf65506d9
Ymanaly <115294312+Ymanaly@users.noreply.github.com>
2022-10-08 18:34:30 +0600

    Add files via upload

---
+++ hw2-Ibragim-19-01/README.md
@@ -0,0 +1,2 @@
+# hw2-Ibragim-19-01
+ b54y5ftt
---
+++ hw2-Ibragim-19-01/hw.js
@@ -0,0 +1,25 @@
+let i = 0;
+while (i < 51) {
+    alert(i);
+    console.log(i);
+    i++;
+}
+
+//Массив
+let arr = ["Привет", "Мир", "!"]
+alert(arr);
+console.log(arr);
+
+//Обьекты
+let user = {
+    name: "Ibragim",
+    age: 19,
+    addres: "Tokyo",
+    device:"Lenovo ThinkBook 14"
+}
+console.log(user);
+
+
+
+
+// 88888888888888888
